5d43e9472664050ddc8dadc3d1cc0052a97ead68
pariwat-earth <ptgslowdown@gmail.com>
2021-08-13 13:20:17 +0700

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ProtfolioComponent } from './protfolio/protfolio.component';
+import { SkillComponent } from './skill/skill.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component:AboutMeComponent
+  },
+  {
+    path: 'skill',
+    component: SkillComponent
+  },
+  {
+    path: 'contact',
+    component: ContactMeComponent
+  },
+  {
+    path: 'protfolio',
+    component: ProtfolioComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
